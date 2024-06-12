4c4f5e360f89320f6d0cc2ad51b919537468b948
Nikita Polovinkin <poloniki@ya.ru>
2024-06-12 22:17:27 +0200

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,27 +10,27 @@ This is a minimal, reproducible example of how to scrape the web with Selenium a
 Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
 """
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
 
-    driver = get_driver()
-    driver.get("http://example.com")
+@st.cache_resource
+def get_driver():
+    return webdriver.Chrome(
+        service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
+        options=options,
+    )
 
-    st.code(driver.page_source)
+
+options = Options()
+options.add_argument("--disable-gpu")
+options.add_argument("--headless")
+
+driver = get_driver()
+driver.get("http://example.com")
+
+st.code(driver.page_source)
